075435f2c8fc948cd6a664f623fbe0fbab87ad6b
Gustau Perez <gustau.perez@gmail.com>
2015-05-21 07:51:43 +0200

    Update cinnamon to 2.6.0.
    It starts the session fine, but I believe cinnamon 2.6 stills
    needs further testing before it can be ported to the ports tree.

--- x11/cinnamon/files/patch-files-usr-lib-cinnamon-settings_cinnamon-settings.py
+++ x11/cinnamon/files/patch-files-usr-lib-cinnamon-settings_cinnamon-settings.py
@@ -1,12 +1,11 @@
---- files/usr/lib/cinnamon-settings/cinnamon-settings.orig.py	2013-02-26 17:20:22.252933982 +0100
-+++ files/usr/lib/cinnamon-settings/cinnamon-settings.py	2013-02-26 17:21:19.081928251 +0100
-@@ -17,7 +17,8 @@
-     if len(mod_files) is 0:
-         raise Exception("No settings modules found!!")
-     for i in range(len(mod_files)):
--        mod_files[i] = mod_files[i].split('/')[5]
-+        mod_files[i] = mod_files[i].split('/')
-+        mod_files[i] = mod_files[i][len(mod_files[i])-1]
-         mod_files[i] = mod_files[i].split('.')[0]
-         if mod_files[i][0:3] != "cs_":
-             raise Exception("Settings modules must have a prefix of 'cs_' !!")
+--- files/usr/lib/cinnamon-settings/cinnamon-settings.py.orig	2015-05-21 07:46:35.023228000 +0200
++++ files/usr/lib/cinnamon-settings/cinnamon-settings.py	2015-05-21 07:46:43.545072000 +0200
+@@ -29,7 +29,7 @@
+     print "No settings modules found!!"
+     sys.exit(1)
+ 
+-mod_files = [x.split('/')[5].split('.')[0] for x in mod_files]
++mod_files = [x.split('/')[6].split('.')[0] for x in mod_files]
+ 
+ for mod_file in mod_files:
+     if mod_file[0:3] != "cs_":
