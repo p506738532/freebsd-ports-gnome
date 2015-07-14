3f2a3ae99c53439e031bedd96b15969362519ba2
Gustau Perez <gustau.perez@gmail.com>
2015-07-14 10:58:52 +0200

    Update mate-system-monitor to 1.10.1
    Remove an unneeded patch. While here sort USE_GNOME.

--- sysutils/mate-system-monitor/files/patch-src_procproperties.cpp
+++ sysutils/mate-system-monitor/files/patch-src_procproperties.cpp
@@ -1,11 +1,11 @@
---- src/procproperties.cpp.orig	2015-01-23 09:15:58.000000000 +0100
-+++ src/procproperties.cpp	2015-04-06 20:55:39.473119000 +0200
+--- src/procproperties.cpp.orig	2015-07-02 23:46:27.000000000 +0200
++++ src/procproperties.cpp	2015-07-14 10:18:48.273918000 +0200
 @@ -26,7 +26,7 @@
  #include <glibtop/procstate.h>
  #if defined (__linux__)
  #include <asm/param.h>
--#elif defined (__OpenBSD__)
-+#elif defined (__OpenBSD__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+-#elif defined (__NetBSD__) || defined (__OpenBSD__)
++#elif defined (__NetBSD__) || defined (__OpenBSD__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
  #include <sys/param.h>
  #include <sys/sysctl.h>
  #endif
@@ -13,8 +13,8 @@
  
      get_process_memory_info(info);
  
--#if defined (__OpenBSD__)
-+#if defined (__OpenBSD__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+-#if defined (__NetBSD__) || defined (__OpenBSD__)
++#if defined (__NetBSD__) || defined (__OpenBSD__) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
      struct clockinfo cinf;
      size_t size = sizeof (cinf);
      int HZ;
