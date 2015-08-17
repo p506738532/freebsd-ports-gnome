01f85bce62cf69a9c90ced2fd4c1634f65f80f6b
Koop Mast <kwm@rainbow-runner.nl>
2015-08-17 02:03:47 +0200

    Update gdm to 3.17.4. Backport the consolekit removal patches so we can use it again. It should work .... testing will commence after 3.17.90 is completed.

---
+++ x11/gdm/files/patch-daemon_gdm-local-display-factory.c
@@ -0,0 +1,138 @@
+--- daemon/gdm-local-display-factory.c.orig	2015-08-16 16:56:01 UTC
++++ daemon/gdm-local-display-factory.c
+@@ -42,6 +42,7 @@
+ 
+ #define GDM_LOCAL_DISPLAY_FACTORY_GET_PRIVATE(o) (G_TYPE_INSTANCE_GET_PRIVATE ((o), GDM_TYPE_LOCAL_DISPLAY_FACTORY, GdmLocalDisplayFactoryPrivate))
+ 
++#define CK_SEAT1_PATH                       "/org/freedesktop/ConsoleKit/Seat1"
+ #define SYSTEMD_SEAT0_PATH                  "seat0"
+ 
+ #define GDM_DBUS_PATH                       "/org/gnome/DisplayManager"
+@@ -59,8 +60,10 @@ struct GdmLocalDisplayFactoryPrivate
+         /* FIXME: this needs to be per seat? */
+         guint            num_failures;
+ 
++#ifdef WITH_SYSTEMD
+         guint            seat_new_id;
+         guint            seat_removed_id;
++#endif
+ };
+ 
+ enum {
+@@ -194,8 +197,20 @@ store_display (GdmLocalDisplayFactory *f
+ static const char *
+ get_seat_of_transient_display (GdmLocalDisplayFactory *factory)
+ {
++        const char *seat_id = NULL;
++
+         /* FIXME: don't hardcode seat */
+-        return SYSTEMD_SEAT0_PATH;
++#ifdef WITH_SYSTEMD
++        if (LOGIND_RUNNING() > 0) {
++                seat_id = SYSTEMD_SEAT0_PATH;
++        }
++#endif
++
++        if (seat_id == NULL) {
++                seat_id = CK_SEAT1_PATH;
++        }
++
++        return seat_id;
+ }
+ 
+ /*
+@@ -220,7 +235,19 @@ gdm_local_display_factory_create_transie
+ 
+         g_debug ("GdmLocalDisplayFactory: Creating transient display");
+ 
+-        display = gdm_local_display_new ();
++#ifdef WITH_SYSTEMD
++        if (LOGIND_RUNNING() > 0) {
++                display = gdm_local_display_new ();
++        }
++#endif
++
++        if (display == NULL) {
++                guint32 num;
++
++                num = take_next_display_number (factory);
++
++                display = gdm_legacy_display_new (num);
++        }
+ 
+         seat_id = get_seat_of_transient_display (factory);
+         g_object_set (display,
+@@ -372,12 +399,14 @@ create_display (GdmLocalDisplayFactory *
+         g_debug ("GdmLocalDisplayFactory: Adding display on seat %s", seat_id);
+ 
+ 
++#ifdef WITH_SYSTEMD
+         if (g_strcmp0 (seat_id, "seat0") == 0) {
+                 display = gdm_local_display_new ();
+                 if (session_type != NULL) {
+                         g_object_set (G_OBJECT (display), "session-type", session_type, NULL);
+                 }
+         }
++#endif
+ 
+         if (display == NULL) {
+                 guint32 num;
+@@ -402,6 +431,8 @@ create_display (GdmLocalDisplayFactory *
+         return display;
+ }
+ 
++#ifdef WITH_SYSTEMD
++
+ static void
+ delete_display (GdmLocalDisplayFactory *factory,
+                 const char             *seat_id) {
+@@ -538,15 +569,27 @@ gdm_local_display_factory_stop_monitor (
+         }
+ }
+ 
++#endif
++
+ static gboolean
+ gdm_local_display_factory_start (GdmDisplayFactory *base_factory)
+ {
+         GdmLocalDisplayFactory *factory = GDM_LOCAL_DISPLAY_FACTORY (base_factory);
++        GdmDisplay             *display;
+ 
+         g_return_val_if_fail (GDM_IS_LOCAL_DISPLAY_FACTORY (factory), FALSE);
+ 
+-        gdm_local_display_factory_start_monitor (factory);
+-        return gdm_local_display_factory_sync_seats (factory);
++#ifdef WITH_SYSTEMD
++        if (LOGIND_RUNNING()) {
++                gdm_local_display_factory_start_monitor (factory);
++                return gdm_local_display_factory_sync_seats (factory);
++        }
++#endif
++
++        /* On ConsoleKit just create Seat1, and that's it. */
++        display = create_display (factory, CK_SEAT1_PATH, NULL, TRUE);
++
++        return display != NULL;
+ }
+ 
+ static gboolean
+@@ -556,7 +599,9 @@ gdm_local_display_factory_stop (GdmDispl
+ 
+         g_return_val_if_fail (GDM_IS_LOCAL_DISPLAY_FACTORY (factory), FALSE);
+ 
++#ifdef WITH_SYSTEMD
+         gdm_local_display_factory_stop_monitor (factory);
++#endif
+ 
+         return TRUE;
+ }
+@@ -703,7 +748,9 @@ gdm_local_display_factory_finalize (GObj
+ 
+         g_hash_table_destroy (factory->priv->used_display_numbers);
+ 
++#ifdef WITH_SYSTEMD
+         gdm_local_display_factory_stop_monitor (factory);
++#endif
+ 
+         G_OBJECT_CLASS (gdm_local_display_factory_parent_class)->finalize (object);
+ }
